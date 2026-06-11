57bd8fa6b8e59722217ade50987ef262d1e534e6
Sanaul islam <57231477+sanaulislam223@users.noreply.github.com>
2026-06-11 20:51:25 +0530

    Update firebase-config.js

--- project/lostFound/firebase-config.js
+++ project/lostFound/firebase-config.js
@@ -1,12 +1,14 @@
+// Firebase configuration (v8 version compatible with our app)
 const firebaseConfig = {
-  apiKey: "AIzaSyCCHg2Q7NGyqQs_t6t_mOLcch4tAuDXOcI",
-  authDomain: "://firebaseapp.com", // ⚡ Yeh sahi value hai
-  projectId: "campus-lost-found-2c88f",
-  storageBucket: "campus-lost-found-2c88f.firebasestorage.app",
-  messagingSenderId: "43257768799",
-  appId: "1:43257768799:web:509fd5ed7cbf53ble5e4ea",
-  measurementId: "G-V9HQBR82Y3"
+    apiKey: "AIzaSyCCHg2Q7NGyqQs_t6t_mOLCch4tAuDXOcI",
+    authDomain: "://firebaseapp.com",
+    projectId: "campus-lost-found-2c88f",
+    storageBucket: "campus-lost-found-2c88f.firebasestorage.app",
+    messagingSenderId: "43257768799",
+    appId: "1:43257768799:web:509fd5ed7cbf53ble5e4ea",
+    measurementId: "G-V9HQBR82Y3"
 };
+
 // Initialize Firebase
 if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
