1a90af48ca4295bf8204830c4e4a46abb38e452a
Sanaul islam <57231477+sanaulislam223@users.noreply.github.com>
2026-06-12 20:48:58 +0530

    Update firebase-config.js

--- project/lostFound/firebase-config.js
+++ project/lostFound/firebase-config.js
@@ -1,16 +1,16 @@
-// Firebase configuration (v8 version compatible with our app)
 const firebaseConfig = {
-    apiKey: "AIzaSyCCHg2Q7NGyqQs_t6t_mOLCch4tAuDXOcI",
-    authDomain: "://firebaseapp.com",
-    projectId: "campus-lost-found-2c88f",
-    storageBucket: "campus-lost-found-2c88f.firebasestorage.app",
-    messagingSenderId: "43257768799",
-    appId: "1:43257768799:web:509fd5ed7cbf53ble5e4ea",
-    measurementId: "G-V9HQBR82Y3"
+  apiKey: "AIzaSyCCHg2Q7NGyqQs_t6t_mOLCch4tAuDX0oI",
+  authDomain: "campus-lost-found-2c88f.firebaseapp.com",
+  projectId: "campus-lost-found-2c88f",
+  storageBucket: "campus-lost-found-2c88f.firebasestorage.app",
+  messagingSenderId: "43257768799",
+  appId: "1:43257768799:web:509fd5ed7cbf53b1e5e4ea",
+  measurementId: "G-V9HQBR82Y3"
 };
 
-// Initialize Firebase
-if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-}
+firebase.initializeApp(firebaseConfig);
+
 const db = firebase.firestore();
+
+console.log("CONFIG LOADED");
+console.log("DB =", db);
