e09333fa410eb220623790a6a6eb6f78b1052fc2
Sanaul islam <57231477+sanaulislam223@users.noreply.github.com>
2026-06-22 16:48:02 +0530

    Update asset path for service worker cache

--- sw.js
+++ sw.js
@@ -1,17 +1,2 @@
-const CACHE_NAME = 'pwa-v1';
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['./index.html', './manifest.json', './icon-512.png']);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
-    })
-  );
-});
+// sw.js ke andar ASSETS wali line aisi honi chahiye:
+return cache.addAll(['./index.html', './manifest.json', './images/icon-512.png']);
